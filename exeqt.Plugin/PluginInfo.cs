0bc59934c70a41be8c04fed9b7731b9ed9462fdb
User <User@DESKTOP-QSJE2HE>
2018-05-04 18:02:58 +0200

    Revert "Revert "Add project basic implementations""
    This reverts commit 7c7beafb97a39986bff23875023d14632c9744ad.

---
+++ exeqt.Plugin/PluginInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exeqt.Plugin
+{
+    public class PluginInfo
+    {
+        public PluginInfo(string name, string description, Version version, PluginType type)
+        {
+            Name = name;
+            Description = description;
+            Version = version;
+            Type = type;
+        }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public Version Version { get; private set; }
+
+        public PluginType Type { get; private set; }
+    }
+}
